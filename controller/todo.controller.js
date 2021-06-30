84c29bad6f7376383ab5a022203416a48c618f86
Shafiq Ur Rehman <69121735+FsCode-shafiq@users.noreply.github.com>
2021-06-30 20:26:23 +0500

    get todo and create todo route completed

--- controller/todo.controller.js
+++ controller/todo.controller.js
@@ -1,3 +1,44 @@
+const {TodoModel} = require('../models');
+
 exports.getTodos = async (req,res)=>{
-    res.json({ sucess: true, message: "User todo work fine" })
+    const {id} = req.user;
+    console.log(id);
+    try {
+        const task = await TodoModel.find({createdBy:id}).populate("createdBy",{password: 0});
+        console.log(task);
+        console.log(task.lenght);
+        if(task.lenght<1){
+            return res.send({
+                sucess: false,
+                message: "you dont have any task"
+            })
+        }
+        res.send({
+            sucess: true,
+            task
+        })
+    } catch (error) {
+        console.log("internal server error: ", error.message);
+    }
+}
+
+exports.createTodo = async(req,res)=>{
+    const {title,description,isCompleted} = req.body;
+
+    try {
+        const todo = await TodoModel.create({
+            title,
+            description,
+            isCompleted,
+            createdBy: req.user.id,
+        });
+        todo.save();
+        res.send({
+            sucess: true,
+            message: "User todo create sucessfully!",
+            todo,
+        })
+    } catch (error) {
+        console.log("internal server error: ", error.message);
+    }
 }
--- routes/todo.router.js
+++ routes/todo.router.js
@@ -1,10 +1,13 @@
 const express = require("express");
 
+const {authmiddleware} = require('../middlewares');
 const { todosControllers } = require('../controller');
 const app = express();
 
 const router = express.Router();
 
-router.get("/", todosControllers.getTodos);
+router.get("/",authmiddleware, todosControllers.getTodos);
+router.post("/create",authmiddleware, todosControllers.createTodo);
+
 
 exports.todoRoute = app.use("/todo", router);
